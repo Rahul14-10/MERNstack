eadc7ffb1676bbe1092e33e8e35d91da2f3de4dc
Rahul14-10 <rahul.moorthy2004@gmail.com>
2025-01-31 16:10:27 +0530

    File Uploaded

---
+++ backend/main.js
@@ -0,0 +1,17 @@
+const connectDb = require("./config/config");
+const express = require("express");
+const cors = require('cors')
+const userRoutes = require("./routes/userRoutes");
+
+connectDb();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/users", userRoutes);
+
+app.listen(3000, () => {
+  console.log("Server is running in PORT: 3000");
+});
